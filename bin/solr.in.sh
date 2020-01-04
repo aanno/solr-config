863dd2bc5db1a755c9c043b712c6efea8bf7c83e
Thomas Pasch <thomas.pasch@gmx.de>
2020-01-04 11:21:09 +0100

    increase Threads for CompletableFutures

--- bin/solr.in.sh
+++ bin/solr.in.sh
@@ -93,6 +93,7 @@
 #SOLR_OPTS="$SOLR_OPTS -Dsolr.autoSoftCommit.maxTime=3000"
 #SOLR_OPTS="$SOLR_OPTS -Dsolr.autoCommit.maxTime=60000"
 #SOLR_OPTS="$SOLR_OPTS -Dsolr.clustering.enabled=true"
+SOLR_OPTS="$SOLR_OPTS -Djava.util.concurrent.ForkJoinPool.common.parallelism=100"
 
 # Location where the bin/solr script will save PID files for running instances
 # If not set, the script will create PID files in $SOLR_TIP/bin
