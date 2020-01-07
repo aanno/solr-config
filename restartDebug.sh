7bbdc8a58088d3dd8a1e054f1fb641b41d6492a2
Thomas Pasch <thomas.pasch@gmx.de>
2020-01-07 09:22:55 +0100

    restartDebug.sh: also compile with ant

--- restartDebug.sh
+++ restartDebug.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -x
 
+SOLR_SRC="/mnt/home/tpasch/scm/github/lucene-solr/solr"
+DI_JAR="$SOLR_SRC/build/contrib/solr-dataimporthandler/solr-dataimporthandler-8.3.2-SNAPSHOT.jar"
+DIE_JAR="$SOLR_SRC/build/contrib/solr-dataimporthandler-extras/solr-dataimporthandler-extras-8.3.2-SNAPSHOT.jar"
+DI_SRC="$SOLR_SRC/contrib/dataimporthandler"
+DIE_SRC="$SOLR_SRC/contrib/dataimporthandler-extras"
+
+pushd "$DI_SRC"
+  ant clean && ant
+popd
+pushd "$DIE_SRC"
+  ant clean && ant
+popd
+
 bin/solr -V stop -all
-cp /mnt/home/tpasch/scm/github/lucene-solr/solr/build/contrib/solr-dataimporthandler/solr-dataimporthandler-8.3.2-SNAPSHOT.jar dist/solr-dataimporthandler-8.3.1.jar
-cp /mnt/home/tpasch/scm/github/lucene-solr/solr/build/contrib/solr-dataimporthandler-extras/solr-dataimporthandler-extras-8.3.2-SNAPSHOT.jar dist/solr-dataimporthandler-extras-8.3.1.jar
+cp "$DI_JAR" dist/solr-dataimporthandler-8.3.1.jar
+cp "$DIE_JAR" dist/solr-dataimporthandler-extras-8.3.1.jar
 cat example/example-DIH/solr/tika/conf/tika-data-config.xml
-bin/solr -V -v -a \"-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=18983 -Xdebug\" -e dih
+bin/solr -V -v -a '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=18983' -a '-Xdebug' -e dih
 less example/example-DIH/logs/solr.log
