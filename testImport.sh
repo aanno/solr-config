9a821b8979993f7a8aed7858c148404329140145
Thomas Pasch <thomas.pasch@gmx.de>
2020-01-09 18:27:46 +0100

    fixes

--- testImport.sh
+++ testImport.sh
@@ -4,7 +4,8 @@ SOLR_SRC="/mnt/home/tpasch/scm/github/lucene-solr/solr"
 IMPORT_SRC="/mnt/home/tpasch/scm/aanno/solr-import-plugin"
 IMPORT_JAR="$IMPORT_SRC/build/libs/solr-import-plugin-all-1.0-SNAPSHOT-all.jar"
 
-curl "http://localhost:8983/solr/tika/update/json?update.chain=import.chain&commit=true" -H 'Content-type: application/json' -d '
+# https://lucene.apache.org/solr/guide/8_4/update-request-processors.html
+curl "http://localhost:8983/solr/tika2/update/json?update.chain=import.chain&commit=true" -H 'Content-type: application/json' -d '
 [
   {
     "name" : "The Lightning Thief",
